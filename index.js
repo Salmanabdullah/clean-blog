56e5693f458598b4851a0ea64a17c827bf236685
Salmanabdullah <salmandcc@gmail.com>
2023-09-10 15:27:44 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const ejs = require("ejs");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("App is listening....");
+});
