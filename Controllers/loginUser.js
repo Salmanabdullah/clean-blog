a1e8096db16d684fae5c765fce2d8339b3e04a60
Salmanabdullah <salmandcc@gmail.com>
2023-10-02 22:18:30 +0200

    resolve password authentication

--- Controllers/loginUser.js
+++ Controllers/loginUser.js
@@ -1,24 +1,23 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { username, password } = req.body;
 
-  User.findOne({ username: username })
-    .then((user,error) => {
-      bcrypt
-        .compare(password, user.password)
-        .then(() => {
-          req.session.userId=user._id
+  await User.findOne({ username: username })
+    .then((user) => {
+      bcrypt.compare(password, user.password, (err, response) => {
+        if (response) {
+          req.session.userId = user._id;
           res.redirect("/");
-        })
-        .catch(() => {
-          console.log('failed');
+        } else {
+          console.log("failed");
           res.redirect("/auth/login");
-        });
+        }
+      });
     })
     .catch(() => {
-      console.log('Login Failed');
+      console.log("Login Failed");
       res.redirect("/auth/login");
     });
 };
--- models/User.js
+++ models/User.js
@@ -12,10 +12,10 @@ const UserSchema = new Schema({
 UserSchema.pre("save", function (next) {
   const user = this;
 
-  bcrypt.hash(user.password, 10, (error, hash) => {
+  bcrypt.hash(user.password, 10).then((hash) => {
     user.password = hash;
-    next();
   });
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
